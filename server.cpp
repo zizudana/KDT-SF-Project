e8f22604625e35eb4322aff06b528d317f9d1a6e
zizudana <zizudana@naver.com>
2023-05-06 12:03:18 +0900

    Edit: 코드정리, 파일분할

--- server.cpp
+++ server.cpp
@@ -1,46 +1,5 @@
 ﻿#pragma comment(lib, "ws2_32.lib")  //명시적인 라이브러리의 링크. 윈속 라이브러리 참조
-
-#include <WinSock2.h>
-#include <string>
-#include <iostream>
-#include <sstream>
-#include <thread>
-#include <vector>
-#include <random>
-#include <functional>
-#include <mysql/jdbc.h>
-
-#define MAX_SIZE 1024
-#define MAX_CLIENT 10
-
-using std::cout;
-using std::cin;
-using std::endl;
-using std::string;
-using std::random_device;
-using std::default_random_engine;
-using std::uniform_int_distribution;
-
-const string server = "tcp://127.0.0.1:3306"; // 데이터베이스 주소
-const string username = "root"; // 데이터베이스 사용자
-const string password = "cho337910!@@"; // 데이터베이스 접속 비밀번호
-
-struct SOCKET_INFO { // 연결된 소켓 정보에 대한 틀 생성
-    SOCKET sck;
-    string user;
-};
-
-std::vector<SOCKET_INFO> sck_list; // 연결된 클라이언트 소켓들을 저장할 배열 선언.
-SOCKET_INFO server_sock; // 서버 소켓에 대한 정보를 저장할 변수 선언.
-int client_count = 0; // 현재 접속해 있는 클라이언트를 count 할 변수 선언.
-
-void server_init(); // socket 초기화 함수. socket(), bind(), listen() 함수 실행됨. 자세한 내용은 함수 구현부에서 확인.
-void add_client(); // 소켓에 연결을 시도하는 client를 추가(accept)하는 함수. client accept() 함수 실행됨. 자세한 내용은 함수 구현부에서 확인.
-void send_msg(const char* msg); // send() 함수 실행됨. 자세한 내용은 함수 구현부에서 확인.
-void recv_msg(int idx); // recv() 함수 실행됨. 자세한 내용은 함수 구현부에서 확인.
-void del_client(int idx); // 소켓에 연결되어 있는 client를 제거하는 함수. closesocket() 실행됨. 자세한 내용은 함수 구현부에서 확인.
-void show_user(int idx);
-void calculator(int idx, int position, string sbuf);
+#include "server.h"
 
 int main() {
     WSADATA wsa;
@@ -80,6 +39,11 @@ int main() {
     }
 
     WSACleanup();
+    // MySQL Connector/C++ 정리
+    delete con;
+    delete stmt;
+    delete pstmt;
+    delete result;
 
     return 0;
 }
@@ -145,13 +109,13 @@ void show_user(int idx) {
 
 void show_func(int idx) {
     string msg = 
-    "-----------------------------------------\n" 
-    "* # :FUNC LIST                          *\n" 
-    "* #USER :현재 접속중인 사용자 아이디    *\n"
-    "* #DM receiver message :DM 보내기       *\n" 
-    "* #CAL number operator number :사칙연산 *\n"
-    "* #GAME : UP & DOWN 게임                *\n"
-    "-----------------------------------------\n";
+    "--------------------------------------------------\n" 
+    "* # : FUNC LIST                                  *\n" 
+    "* #USER :현재 접속중인 사용자 아이디             *\n"
+    "* #DM receiver message : DM 보내기               *\n" 
+    "* #CAL number operator number : 사칙연산         *\n"
+    "* #GAME : UP & DOWN 게임                         *\n"
+    "--------------------------------------------------\n";
     send(sck_list[idx].sck, msg.c_str(), MAX_SIZE, 0);
     cout << msg << endl;
 }
@@ -184,15 +148,33 @@ void calculator(int idx, int position, string sbuf) {
 
 }
 
+void send_dm(int position, string sbuf, int idx) {
+    int cur_position = position + 1;
+    position = sbuf.find(" ", cur_position);
+    int len = position - cur_position;
+    string receiver = sbuf.substr(cur_position, len);
+    cur_position = position + 1;
+    string dm = sbuf.substr(cur_position);
+    string msg = "[DM] " + sck_list[idx].user + " : " + dm;
+    pstmt = con->prepareStatement("INSERT INTO chatting(sender,receiver, message) VALUES(?,?,?)"); // INSERT
+    pstmt->setString(1, sck_list[idx].user);
+    pstmt->setString(2, receiver);
+    pstmt->setString(3, dm);
+    pstmt->execute(); // 쿼리 실행
+    pstmt = con->prepareStatement("SELECT time FROM chatting ORDER BY time DESC LIMIT 1");
+    result = pstmt->executeQuery();
+
+    while (result->next()) {
+        msg += " [" + result->getString(1) + "]\n";
+    }
+    cout << msg << endl;
+    for (int i = 0; i < client_count; i++) {
+        if (receiver.compare(sck_list[i].user) == 0)
+            send(sck_list[i].sck, msg.c_str(), MAX_SIZE, 0);
+    }
+}
+
 void recv_msg(int idx) {
-
-    // MySQL Connector/C++ 초기화
-    sql::mysql::MySQL_Driver* driver; // 추후 해제하지 않아도 Connector/C++가 자동으로 해제해 줌
-    sql::Connection* con;
-    sql::Statement* stmt;
-    sql::PreparedStatement* pstmt;
-    sql::ResultSet* result;
-
     try {
         driver = sql::mysql::get_mysql_driver_instance();
         con = driver->connect(server, username, password);
@@ -217,9 +199,8 @@ void recv_msg(int idx) {
         ZeroMemory(&buf, MAX_SIZE);
         if (recv(sck_list[idx].sck, buf, MAX_SIZE, 0) > 0) { // 오류가 발생하지 않으면 recv는 수신된 바이트 수를 반환. 0보다 크다는 것은 메시지가 왔다는 것.
             string sbuf(buf);
-            if (sbuf.compare("#") == 0) {
+            if (sbuf.compare("#") == 0)
                 show_func(idx);
-            }
             else if (sbuf.compare("#USER") == 0)
                 show_user(idx);
             else if (buf[0] == '#') {
@@ -227,28 +208,10 @@ void recv_msg(int idx) {
                 int position = sbuf.find(" ", cur_position);
                 int len = position - cur_position;
                 string flag = sbuf.substr(cur_position, len);
-                if (flag.compare("#DM") == 0) {
-                    cur_position = position + 1;
-                    position = sbuf.find(" ", cur_position);
-                    len = position - cur_position;
-                    string receiver = sbuf.substr(cur_position, len);
-                    cur_position = position + 1;
-                    string dm = sbuf.substr(cur_position);
-                    msg = "[DM] " + sck_list[idx].user + " : " + dm;
-                    pstmt = con->prepareStatement("INSERT INTO chatting(sender,receiver, message) VALUES(?,?,?)"); // INSERT
-                    pstmt->setString(1, sck_list[idx].user);
-                    pstmt->setString(2, receiver);
-                    pstmt->setString(3, dm);
-                    pstmt->execute(); // 쿼리 실행
-                    cout << msg << endl;
-                    for (int i = 0; i < client_count; i++) {
-                        if (receiver.compare(sck_list[i].user) == 0)
-                            send(sck_list[i].sck, msg.c_str(), MAX_SIZE, 0); 
-                    }
-                }
-                else if (flag.compare("#CAL") == 0) {
+                if (flag.compare("#DM") == 0)
+                    send_dm(position, sbuf, idx);
+                else if (flag.compare("#CAL") == 0)
                     calculator(idx, position, sbuf);
-                }
             }
             else {
                 msg = sck_list[idx].user + " : " + buf;
@@ -264,8 +227,6 @@ void recv_msg(int idx) {
                 }
                     cout << msg << endl;
                 send_msg(msg.c_str());
-               
-                
             }
         }
         else { //그렇지 않을 경우 퇴장에 대한 신호로 생각하여 퇴장 메시지 전송
